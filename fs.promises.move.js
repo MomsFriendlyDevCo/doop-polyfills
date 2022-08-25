d447ce24c49bd3d4c7e53832206d9b6f135b9b47
Matt Carter <m@ttcarter.com>
2022-08-25 16:01:51 +1000

    feat: fs.promises.move() now also supports recursive directories

--- fs.promises.move.js
+++ fs.promises.move.js
@@ -1,27 +1,38 @@
-var fs = require('fs')
+let fs = require('fs')
 
 /**
 * Attempt to rename a file if its on the same device OR move it if not
 *
 * @param {string} src Source path to move from
 * @param {string} dst Destination path to move to
+* @param {Object} [options] Additional options to configure behaviour
+* @param {boolean} [options.mkdir=true] Create destination directories before moving
 * @returns {Promise<string>} A promise which will resolve with the destination path when the file has been moved
 */
-fs.promises.move = (src, dst) =>
-	fs.promises.rename(src, dst).catch(e => { // Try a simple rename first
-		if (e.code != 'EXDEV') throw e; // Not a "not the same device" error - throw it and die
+fs.promises.move = (src, dst, options) => {
+	let settings = {
+		mkdir: true,
+		...options,
+	};
 
-		return new Promise((resolve, reject) => { // Create a copy + unlink the original
-			var readStream = fs.createReadStream(src);
-			var writeStream = fs.createWriteStream(dst);
+	return Promise.resolve()
+		.then(()=> settings.mkdir && fs.promises.mkdir(fspath.dirname(dst), {recursive: true}))
+		.then(()=> fs.promises.rename(src, dst).catch(e => { // Try a simple rename first
+			if (e.code != 'EXDEV') throw e; // Not a "not the same device" error - throw it and die
 
-			readStream.on('error', reject);
-			writeStream.on('error', reject);
+			return Promise.resolve()
+				.then(()=> new Promise((resolve, reject) => { // Create a copy + unlink the original
+					var readStream = fs.createReadStream(src);
+					var writeStream = fs.createWriteStream(dst);
 
-			writeStream.on('finish', ()=> resolve());
+					readStream.on('error', reject);
+					writeStream.on('error', reject);
 
-			readStream.pipe(writeStream);
-		})
-			.then(()=> fs.promises.unlink(src))
-			.then(()=> dst)
-	})
+					writeStream.on('finish', ()=> resolve());
+
+					readStream.pipe(writeStream);
+				}))
+				.then(()=> fs.promises.unlink(src))
+				.then(()=> dst)
+		}))
+};
