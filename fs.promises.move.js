4a1aedf976510f7aac4a42ef5adf3a4578d9387a
Matt Carter <m@ttcarter.com>
2022-08-27 09:27:26 +1000

    fix: Missing dep

--- fs.promises.move.js
+++ fs.promises.move.js
@@ -1,4 +1,5 @@
 let fs = require('fs')
+let fsPath = require('path');
 
 /**
 * Attempt to rename a file if its on the same device OR move it if not
@@ -16,7 +17,7 @@ fs.promises.move = (src, dst, options) => {
 	};
 
 	return Promise.resolve()
-		.then(()=> settings.mkdir && fs.promises.mkdir(fspath.dirname(dst), {recursive: true}))
+		.then(()=> settings.mkdir && fs.promises.mkdir(fsPath.dirname(dst), {recursive: true}))
 		.then(()=> fs.promises.rename(src, dst).catch(e => { // Try a simple rename first
 			if (e.code != 'EXDEV') throw e; // Not a "not the same device" error - throw it and die
 
